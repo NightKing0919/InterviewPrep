4a8dc1189795e3e7c9344d295cbfebc75567016c
SandViper <svamsiiscrazy@gmail.com>
2018-11-27 01:51:33 +0530

    Updated Main--Quick Union done.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
-import Algorithms.UnionFind.QuickFindUF;
+import Algorithms.UnionFind.QuickUnionUF;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        QuickFindUF uf=new QuickFindUF(4);
-        uf.union(0,1);uf.union(2,3);
-        System.out.println(uf.connected(0,0));
+        QuickUnionUF uf=new QuickUnionUF(10);
+        uf.union(2,9);
+        uf.union(4,9);
+        uf.union(4,3);
+        uf.union(6,5);
+
+        System.out.println(uf.connected(5,3));
+        uf.union(4,6);
+
+        System.out.println(uf.connected(5,3));
     }
 }
